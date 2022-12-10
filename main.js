b7d4f30052c6d82e3330bce259c46ed515ae6366
Jaredzu <jared.zu96@gmail.com>
2022-12-10 12:06:31 -0500

    first Structure of Countdown

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hello Countdown💜")
+
+const month = document.querySelector("#month")
+const day = document.querySelector("#day")
+const hour = document.querySelector("#hour")
+const minute = document.querySelector("#minute")
+const seconds = document.querySelector("#seconds")
+
+const actualDate = new Date()
+console.log(`El día de hoy: ${actualDate}`)
+
+const finalDate = new Date('Jan 01 2023')
+console.log(`Fecha final: ${finalDate}`)
+
+
