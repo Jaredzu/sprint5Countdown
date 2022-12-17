9fee712bd6d1960b516eece536f4d7b69ee5c350
Jaredzu <jared.zu96@gmail.com>
2022-12-17 16:45:24 -0500

    Create more js

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 console.log("Hello Countdown💜")
 
-let monthOutput = document.querySelector("#month")
-let dayOutput = document.querySelector("#day")
-let hourOutput = document.querySelector("#hour")
-let minuteOutput = document.querySelector("#minute")
-let secondsOutput = document.querySelector("#seconds")
+import { updateCountDown } from "./countDown.js";
 
 const finalDate = new Date('Dec 31 2023 23:59:59')
 console.log(`Fecha final: ${finalDate}`)
-
-const updateCountDown = (time) => {
-    let month = Math.floor(time / 1000 / 60 / 60 / 24 / 30)
-    let day = Math.floor(time / 1000 / 60 / 60 / 24) % 30
-    let hour = Math.floor(time / 1000 / 60 / 60) % 24
-    let minute = Math.floor(time / 1000 / 60) % 60
-    let seconds = Math.floor(time / 1000) % 60
-
-    monthOutput.innerHTML = month < 10 ? `0${month}` : month;
-    dayOutput.innerHTML = day < 10 ? `0${day}` : day;
-    hourOutput.innerHTML = hour < 10 ? `0${hour}` : hour;
-    minuteOutput.innerHTML = minute < 10 ? `0${minute}` : minute;
-    secondsOutput.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-}
 
 setInterval(() => {
     const actualDate = new Date()
